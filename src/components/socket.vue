90ade4486cb651ba3f575b34a3fcbfc73676fae3
yanglx <1071126434@qq.com>
2018-03-06 14:47:04 +0800

    https

--- src/components/socket.vue
+++ src/components/socket.vue
@@ -122,7 +122,7 @@ import Vue from 'vue'
 import VueSocketio from 'vue-socket.io'
 /* global BMap */
 /* eslint no-undef: "error" */
-Vue.use(VueSocketio, 'http://localhost:8889')
+Vue.use(VueSocketio, 'http://127.0.0.1:8889')
 export default {
   name: 'HelloWorld',
   data () {
